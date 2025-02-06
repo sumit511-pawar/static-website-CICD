5172bdc905d90f4f7bddf8664dd0cf99e94610cb
sumit pawar <pawarsumitp2000@gmail.com>
2025-02-06 05:06:57 -0800

    Added tech startup landing page

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById("clickMeBtn").addEventListener("click", function() {
-    alert("Button Clicked!");
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("mouseover", function () {
+            button.style.transform = "scale(1.1)";
+        });
+
+        button.addEventListener("mouseleave", function () {
+            button.style.transform = "scale(1)";
+        });
+    });
 });
